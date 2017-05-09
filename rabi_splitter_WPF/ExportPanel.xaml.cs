ecc8afbab60d660653674d1a5ed78ad30905ec7d
wcko87 <wcko87@gmail.com>
2017-05-09 08:38:56 +0800

    Some groundwork for variable export

---
+++ rabi_splitter_WPF/ExportPanel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rabi_splitter_WPF
+{
+    /// <summary>
+    /// Interaction logic for ExportPanel.xaml
+    /// </summary>
+    public partial class ExportPanel : UserControl
+    {
+        public object VariableExportObject
+        {
+            get { return (object)GetValue(VariableExportObjectProperty); }
+            set { SetValue(VariableExportObjectProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty VariableExportObjectProperty =
+            DependencyProperty.Register("VariableExportObject", typeof(object),
+              typeof(VariableExportSetting), new PropertyMetadata(null));
+
+        public ExportPanel()
+        {
+            InitializeComponent();
+            this.MainPanel.DataContext = this;
+        }
+    }
+}
---
+++ rabi_splitter_WPF/VariableExportSetting.cs
@@ -0,0 +1,108 @@
+﻿using rabi_splitter_WPF.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace rabi_splitter_WPF
+{
+    class ExportableVariable
+    {
+
+
+        public List<ExportableVariable> GetAll()
+        {
+            return new List<ExportableVariable>();
+        }
+    }
+
+    class VariableExportSetting : INotifyPropertyChanged
+    {
+        private ExportableVariable _selectedVariable;
+        private string _outputFileName;
+        private string _outputFormat;
+        private bool _isExporting;
+
+        #region Dictionaries 
+        
+        // Captions for Split Trigger Options
+        private static readonly Dictionary<ExportableVariable, string> _variableCaptions = new Dictionary<ExportableVariable, string>()
+        {
+        };
+        
+        public Dictionary<ExportableVariable, string> VariableCaptions
+        {
+            get {return _variableCaptions;}
+        }
+        
+        #endregion
+        
+        public VariableExportSetting()
+        {
+            // Default values
+            _selectedVariable = null;
+            _outputFileName = "";
+            _outputFormat = "";
+            _isExporting = false;
+        }
+
+        #region Parameters
+
+        public ExportableVariable SelectedVariable
+        {
+            get { return _selectedVariable; }
+            set
+            {
+                if (value.Equals(_selectedVariable)) return;
+                _selectedVariable = value;
+                OnPropertyChanged(nameof(SelectedVariable));
+            }
+        }
+
+        public string OutputFileName
+        {
+            get { return _outputFileName; }
+            set
+            {
+                if (value.Equals(_outputFileName)) return;
+                _outputFileName = value;
+                OnPropertyChanged(nameof(OutputFileName));
+            }
+        }
+
+        public string OutputFormat
+        {
+            get { return _outputFormat; }
+            set
+            {
+                if (value.Equals(_outputFormat)) return;
+                _outputFormat = value;
+                OnPropertyChanged(nameof(OutputFormat));
+            }
+        }
+
+        public bool IsExporting
+        {
+            get { return _isExporting; }
+            set
+            {
+                if (value.Equals(_isExporting)) return;
+                _isExporting = value;
+                OnPropertyChanged(nameof(IsExporting));
+            }
+        }
+
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
